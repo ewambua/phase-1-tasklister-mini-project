7b7b19bb68de4f18c36c3394ca0b6f1a829962d5
EmmanuelWambua <emmanuelwambua931@gmail.com>
2023-03-21 20:55:26 +0300

    Done with Lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskForm = () => document.querySelector('form')
+  taskForm().addEventListener('submit', createNewTask)
 });
+
+function createNewTask(e) {
+  e.preventDefault()
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+  let deleteButton = document.createElement('button')
+  deleteButton.textContent = 'remove'
+
+  const todoList = document.getElementById("tasks");
+  todoList.appendChild(newTask);
+  todoList.appendChild(deleteButton);
+  deleteButton.addEventListener('click', removeTask)
+
+  li.appendChild(deleteButton)
+  todoList().appendChild(li)
+
+  e.target.reset();
+}
+
+function removeTask(e){
+  e.target.parentNode.remove();
+}
